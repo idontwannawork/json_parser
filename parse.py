4bd236fb55e5e70a4fcad831459af8c411f579b8
idontwannawork <42630040+idontwannawork@users.noreply.github.com>
2018-11-27 12:20:06 +0900

    Add files via upload

--- parse.py
+++ parse.py
@@ -1,13 +1,22 @@
 # -*- coding: utf8 -*-
 
 import json
+import sys
 
-with open('blacklist.json', mode='r', encoding='utf-8') as fi:
-    json_dict = json.load(fi)
+def putpaser(filepath):
+    
+    with open(filepath, mode='r', encoding='utf-8') as fi:
+        json_dict = json.load(fi)
+
+    return json.dumps(json_dict, ensure_ascii=False, indent=4)
 
 # ファイル出力する場合
-with open('parsed.json',mode='w', encoding='utf-8') as fo:
-    json.dump(json_dict, fo, ensure_ascii=False, indent=4)
+# with open('parsed.json,mode='w', encoding='utf-8') as fo:
+#     json.dump(json_dict, fo, ensure_ascii=False, indent=4)
 
 # コンソール出力する場合
-print(json.dumps(json_dict, ensure_ascii=False, indent=4))
+# print(json.dumps(json_dict, ensure_ascii=False, indent=4))
+
+args = sys.argv
+
+print(putpaser(args[1]))
